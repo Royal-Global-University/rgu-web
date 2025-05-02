8a42ce82be244d33231741543acd12b2de824b74
Royal-Global-University <ahussain1@rgu.ac>
2025-05-02 21:08:01 +0530

    ip chnage 3

--- resources/views/frontend/components/ip.blade.php
+++ resources/views/frontend/components/ip.blade.php
@@ -10,39 +10,39 @@
             <ul class="navbar-nav" style="width: 100%; justify-content: space-around;">
 
                 <!-- Discover RGU -->
-                <li class="nav-item dropdown" style="position: relative;">
-                    <a class="nav-link dropdown-toggle para1" href="#"
+                <li style="position: relative;">
+                    <a href="#"
                         style="color: #000000; font-weight: 600; font-size: 16px;">Why RGU</a>
                 </li>
 
                 <!-- Academics -->
-                <li class="nav-item dropdown" style="position: relative;">
-                    <a class="nav-link dropdown-toggle para1" href="#"
+                <li style="position: relative;">
+                    <a href="#"
                         style="color: #000000; font-weight: 600; font-size: 16px;">Programmes Offered</a>
                 </li>
 
                 <!-- Admissions -->
-                <li class="nav-item dropdown" style="position: relative;">
-                    <a class="nav-link dropdown-toggle para1" href="#"
+                <li style="position: relative;">
+                    <a href="#"
                         style="color: #000000; font-weight: 600; font-size: 16px;">Virtual Campus Tour</a>
                 </li>
 
                 <!-- Convocation -->
-                <li class="nav-item dropdown" style="position: relative;">
-                    <a class="nav-link dropdown-toggle para1" href="#"
+                <li style="position: relative;">
+                    <a href="#"
                         style="color: #000000; font-weight: 600; font-size: 16px;">Life @ RGU</a>
                 </li>
 
                 <!-- Campus Life with Submenu -->
-                <li class="nav-item dropdown" style="position: relative;">
-                    <a class="nav-link dropdown-toggle para1" href="#"
+                <li style="position: relative;">
+                    <a href="#"
                         style="color: #000000; font-weight: 600; font-size: 16px;">Life @ RGU</a>
                 </li>
 
                 <!-- Placements -->
-                <li class="nav-item dropdown" style="position: relative;">
-                    <a class="nav-link dropdown-toggle para1" href="#"
-                        style="color: #ffffff; font-weight: 600; font-size: 16px;">International Admissions Open - 2024</a>
+                <li style="position: relative;">
+                    <a href="#" class="p-3"
+                        style="color: #ffffff; font-weight: 600; font-size: 16px; background-color: #ff5e3a;">International Admissions Open - 2024</a>
                 </li>
 
             </ul>
--- resources/views/frontend/lp/lp.blade.php
+++ resources/views/frontend/lp/lp.blade.php
@@ -23,7 +23,7 @@
               </div>
 
               <div class="col-lg-5 npf_wgts-main pt-5" style="z-index: 1000">
-                <div class="npf_wgts" data-height="400px" data-w="30f6f0f5207a8612717c00304293b304"></div>
+                <div class="npf_wgts" data-height="700px" data-w="30f6f0f5207a8612717c00304293b304"></div>
               </div>
 
             </div>
