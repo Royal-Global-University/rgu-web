55bb46827ed7a68bb4639132d17bc05bd1221978
Royal-Global-University <ahussain1@rgu.ac>
2025-05-02 21:08:48 +0530

    ip chnage4

--- resources/views/frontend/lp/lp.blade.php
+++ resources/views/frontend/lp/lp.blade.php
@@ -22,8 +22,8 @@
                 </div>
               </div>
 
-              <div class="col-lg-5 npf_wgts-main pt-5" style="z-index: 1000">
-                <div class="npf_wgts" data-height="700px" data-w="30f6f0f5207a8612717c00304293b304"></div>
+              <div class="col-lg-5 npf_wgts-main pt-2" style="z-index: 1000">
+                <div class="npf_wgts" data-height="650px" data-w="30f6f0f5207a8612717c00304293b304"></div>
               </div>
 
             </div>
