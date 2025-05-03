d65ff32d02ebb37c31ff2c441cd7c3084851a135
Royal-Global-University <ahussain1@rgu.ac>
2025-05-03 16:49:32 +0530

    new page8

--- resources/views/frontend/lp/lp.blade.php
+++ resources/views/frontend/lp/lp.blade.php
@@ -9,8 +9,8 @@
 
       <div class="mobile-hero">
           <div class="row">
-            <div class="col-lg-12">
-              <div class="npf_wgts" data-height="400px" data-w="30f6f0f5207a8612717c00304293b304"></div>
+            <div class="col-lg-12" style="z-index: 1000;">
+              <div class="npf_wgts" data-height="680px" data-w="30f6f0f5207a8612717c00304293b304"></div>
             </div>
           </div>
       </div>
