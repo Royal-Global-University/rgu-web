822b22aa5b8d3538d6d214319b76bbc064c04bd8
Royal-Global-University <ahussain1@rgu.ac>
2025-05-05 12:12:42 +0530

    new hero 4

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -560,7 +560,7 @@
         .card-box {
             border: 1px solid #e0e0e0;
             border-radius: 10px;
-            padding: 40px 20px;
+            padding: 40px 5px;
             display: flex;
             justify-content: space-between;
             position: relative;
