f36465741fa1e7309c6418b633a7ba4a232ebdac
Royal-Global-University <ahussain1@rgu.ac>
2025-05-13 15:17:57 +0530

    icc

--- resources/views/frontend/discoverRGU/internal-complaints-committee.blade.php
+++ resources/views/frontend/discoverRGU/internal-complaints-committee.blade.php
@@ -2,104 +2,117 @@
 @section('content')
     <div class="mobile">
         @include('frontend/components/mobileheader')
-        <a style="padding-top: 90px; position: relative;" target="_blank" href="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/Gender-Sentization-Cell.pdf"> <img src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/web-icc.png" /></a>
+        <div style="padding-top: 80px; position: relative;">
+            <a target="_blank"
+                href="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/Gender-Sentization-Cell.pdf"> <img
+                    src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/web-icc.png" /></a>
 
-     <div style="background-image: url(https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/bg.svg); background-size: cover; padding: 20px 0px;">
-                     <div class="container">
-                         <div class="text-center pb-4">
-                      <img style="width: 800px;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/meet.png" />
-                      </div>
-                      <p class="para1" style="text-align: justify; color: #13223b;">
-                         In regard to the Internal Complaints Committee (ICC), RGU as per the University Grants Commission (Prevention,
-                         prohibition and redressal of sexual harassment of women employees and students in higher educational institutions)
-                         Regulations, 2015, the committee has been reconstituted with the following members. The tenure of the committee
-                         shall be for three years or until further notification, whichever is earlier.
-                      </p>
-                     </div>
-     </div>
+            <div
+                style="background-image: url(https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/bg.svg); background-size: cover; padding: 20px 0px;">
+                <div class="container">
+                    <div class="text-center pb-4">
+                        <img style="width: 800px;"
+                            src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/internal-cc/meet.png" />
+                    </div>
+                    <p class="para1" style="text-align: justify; color: #13223b;">
+                        In regard to the Internal Complaints Committee (ICC), RGU as per the University Grants Commission
+                        (Prevention,
+                        prohibition and redressal of sexual harassment of women employees and students in higher educational
+                        institutions)
+                        Regulations, 2015, the committee has been reconstituted with the following members. The tenure of the
+                        committee
+                        shall be for three years or until further notification, whichever is earlier.
+                    </p>
+                </div>
+            </div>
 
-     <div class="container pt-3">
+            <div class="container pt-3">
 
-         <p class="text-dark para1 fw-bold">
-             Following are the members of Internal Complaints Committee (ICC), RGU –
-         </p>
-                 <div class="table-responsive">
-                 <table class="table border table-responsive table-striped fs-16 vertical-scroll">
-                     <thead class="text-light  para1" style="font-size: 14px;">
-                         <tr class="text-white para1 text-dark" style="background-color: #005273;">
-                              <th scope="col">Sl No</th>
-                             <th scope="col">Name of the Staff / Faculty</th>
-                             <th scope="col">Category of Nomination</th>
-                             <th scope="col">Designation in ICC</th>
-                             <th scope="col">Tenure</th>
-                         </tr>
-                     </thead>
-                     <tbody class="para1 text-dark">
-                          <tr>
-         <td>1</td>
-         <td>Prof. Indrani Rai, Dean RSL</td>
-         <td>Presiding Officer who shall be a woman faculty member employed at a senior level</td>
-         <td>Presiding Officer</td>
-         <td>9th May 2025 to 8th May 2028</td>
-       </tr>
-       <tr>
-         <td>2</td>
-         <td>Ms. Angira Mimani, Associate Professor & Associate Dean, Student Welfare</td>
-         <td>Two faculty members</td>
-         <td>Member</td>
-         <td>9th May 2025 to 8th May 2028</td>
-       </tr>
-       <tr>
-         <td>3</td>
-         <td>Dr. Debarupa Dutta Chakraborty, Associate Professor, Royal School of Pharmacy</td>
-         <td>Two faculty members</td>
-         <td>Member</td>
-         <td>9th May 2025 to 8th May 2028</td>
-       </tr>
-       <tr>
-         <td>4</td>
-         <td>Mr. Shamim A. Goney, Deputy Registrar</td>
-         <td>Two non-teaching employees</td>
-         <td>Member</td>
-         <td>9th May 2025 to 8th May 2028</td>
-       </tr>
-       <tr>
-         <td>5</td>
-         <td>Ms. Dipika T. Agarwal, Deputy Registrar (HR & RC)</td>
-         <td>Two non-teaching employees</td>
-         <td>Member-Secretary</td>
-         <td>4th Aug 2023 to 3rd Aug 2026</td>
-       </tr>
-       <tr>
-         <td>6</td>
-         <td>Dr. Jwmwishree Boro, Assistant Professor, Psychology</td>
-         <td>Extra Member</td>
-         <td>Member</td>
-         <td>4th Aug 2023 to 3rd Aug 2026</td>
-       </tr>
-       <tr>
-         <td>7</td>
-         <td>Prof. (Dr.) Soumitra Sen, Dean, RSTTM & RSHM</td>
-         <td>Extra Member</td>
-         <td>Member</td>
-         <td>4th Aug 2023 to 3rd Aug 2026</td>
-       </tr>
-                     </tbody>
-                 </table>
-                 </div>
+                <p class="text-dark para1 fw-bold">
+                    Following are the members of Internal Complaints Committee (ICC), RGU –
+                </p>
+                <div class="table-responsive">
+                    <table class="table border table-responsive table-striped fs-16 vertical-scroll">
+                        <thead class="text-light  para1" style="font-size: 14px;">
+                            <tr class="text-white para1 text-dark" style="background-color: #005273;">
+                                <th scope="col">Sl No</th>
+                                <th scope="col">Name of the Staff / Faculty</th>
+                                <th scope="col">Category of Nomination</th>
+                                <th scope="col">Designation in ICC</th>
+                                <th scope="col">Tenure</th>
+                            </tr>
+                        </thead>
+                        <tbody class="para1 text-dark">
+                            <tr>
+                                <td>1</td>
+                                <td>Prof. Indrani Rai, Dean RSL</td>
+                                <td>Presiding Officer who shall be a woman faculty member employed at a senior level</td>
+                                <td>Presiding Officer</td>
+                                <td>9th May 2025 to 8th May 2028</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Ms. Angira Mimani, Associate Professor & Associate Dean, Student Welfare</td>
+                                <td>Two faculty members</td>
+                                <td>Member</td>
+                                <td>9th May 2025 to 8th May 2028</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Dr. Debarupa Dutta Chakraborty, Associate Professor, Royal School of Pharmacy</td>
+                                <td>Two faculty members</td>
+                                <td>Member</td>
+                                <td>9th May 2025 to 8th May 2028</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Mr. Shamim A. Goney, Deputy Registrar</td>
+                                <td>Two non-teaching employees</td>
+                                <td>Member</td>
+                                <td>9th May 2025 to 8th May 2028</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Ms. Dipika T. Agarwal, Deputy Registrar (HR & RC)</td>
+                                <td>Two non-teaching employees</td>
+                                <td>Member-Secretary</td>
+                                <td>4th Aug 2023 to 3rd Aug 2026</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Dr. Jwmwishree Boro, Assistant Professor, Psychology</td>
+                                <td>Extra Member</td>
+                                <td>Member</td>
+                                <td>4th Aug 2023 to 3rd Aug 2026</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Prof. (Dr.) Soumitra Sen, Dean, RSTTM & RSHM</td>
+                                <td>Extra Member</td>
+                                <td>Member</td>
+                                <td>4th Aug 2023 to 3rd Aug 2026</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-     </div>
+            </div>
 
-     <div class="container mb-5">
-             <div class="container" style="background: #1f3b6b; color: white; padding: 15px; border-radius: 10px; flex-wrap: wrap; text-align: center;">
-                 <p class="mobile-mobile-headd3" style="font-weight: bold;">For any query:</p>
-                 <p class="mobile-para1"><span style="font-weight: bold;">Ph. no:</span> <a href="tel: 9678009412" style="color: white; text-decoration: none;">+91 9678009412</a></p>
-                 <p class="mobile-para1"><span style="font-weight: bold;">Email:</span> <a href="mailto: icc@rgu.ac" style="color: white; text-decoration: none;">icc@rgu.ac</a></p>
-                 <a href="tel: 9678009412" class="mobile-call-btn fw-bold" style="text-decoration: none; background: #FF9A1E; color: white; padding: 8px 20px; border-radius: 5px; font-size: 18px; margin-top: 10px; display: inline-block;">
-                     Call Now
-                 </a>
-             </div>
-     </div>
+            <div class="container mb-5">
+                <div class="container"
+                    style="background: #1f3b6b; color: white; padding: 15px; border-radius: 10px; flex-wrap: wrap; text-align: center;">
+                    <p class="mobile-mobile-headd3" style="font-weight: bold;">For any query:</p>
+                    <p class="mobile-para1"><span style="font-weight: bold;">Ph. no:</span> <a href="tel: 9678009412"
+                            style="color: white; text-decoration: none;">+91 9678009412</a></p>
+                    <p class="mobile-para1"><span style="font-weight: bold;">Email:</span> <a href="mailto: icc@rgu.ac"
+                            style="color: white; text-decoration: none;">icc@rgu.ac</a></p>
+                    <a href="tel: 9678009412" class="mobile-call-btn fw-bold"
+                        style="text-decoration: none; background: #FF9A1E; color: white; padding: 8px 20px; border-radius: 5px; font-size: 18px; margin-top: 10px; display: inline-block;">
+                        Call Now
+                    </a>
+                </div>
+            </div>
+        </div>
 
     </div>
 
