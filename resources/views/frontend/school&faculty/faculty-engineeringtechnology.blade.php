aa711ac203000f87fcaac789df4502d94da6c624
Royal-Global-University <ahussain1@rgu.ac>
2025-05-20 17:35:17 +0530

    civil faculty border

--- resources/views/frontend/school&faculty/faculty-engineeringtechnology.blade.php
+++ resources/views/frontend/school&faculty/faculty-engineeringtechnology.blade.php
@@ -224,7 +224,7 @@
 
                         <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 510px;">
                             <div class="p-3">
-                              <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/PIC.png" class="img-fluid rounded"/>
+                              <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/PIC.png" class="img-fluid rounded border"/>
                             </div>
                             <div class="card-body d-flex flex-column flex-grow-1">
                               <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Prof. (Dr.) Arnab Sarma</h5>
