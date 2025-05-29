33de98ab7104fd20ec9db7e61ea7dc65c7b36c4b
Royal-Global-University <ahussain1@rgu.ac>
2025-05-29 11:20:08 +0530

    travel

--- resources/views/frontend/school&faculty/schools-rgu.blade.php
+++ resources/views/frontend/school&faculty/schools-rgu.blade.php
@@ -570,9 +570,9 @@
                         <a href="{{route('department-travel')}}" style="background-color: #27467A; border: none; color: #fff; font-weight: 600; border-radius: 8px; padding: 10px 0; text-align: center; width: 48%; text-decoration: none; display: inline-block;">
                           Department
                         </a>
-                        {{-- <a href="{{route('faculty-travel-tourism')}}" style="background-color: #FF9A1E; border: none; color: #fff; font-weight: 600; border-radius: 8px; padding: 10px 0; text-align: center; width: 48%; text-decoration: none; display: inline-block;">
+                        <a href="{{route('faculty-travel-tourism')}}" style="background-color: #FF9A1E; border: none; color: #fff; font-weight: 600; border-radius: 8px; padding: 10px 0; text-align: center; width: 48%; text-decoration: none; display: inline-block;">
                           Faculty
-                        </a> --}}
+                        </a>
                       </div>
                     </div>
                   </div>
@@ -1177,9 +1177,9 @@
                       <a class="para1" href="{{route('department-travel')}}" style="background-color: #27467A; border: none; color: #fff; font-weight: 400; border-radius: 8px; padding: 10px 0; text-align: center; width: 48%; text-decoration: none; display: inline-block;">
                         Department
                       </a>
-                      {{-- <a class="para1" href="{{route('faculty-travel-tourism')}}" style="background-color: #FF9A1E; border: none; color: #fff; font-weight: 400; border-radius: 8px; padding: 10px 0; text-align: center; width: 48%; text-decoration: none; display: inline-block;">
+                      <a class="para1" href="{{route('faculty-travel-tourism')}}" style="background-color: #FF9A1E; border: none; color: #fff; font-weight: 400; border-radius: 8px; padding: 10px 0; text-align: center; width: 48%; text-decoration: none; display: inline-block;">
                         Faculty
-                      </a> --}}
+                      </a>
                     </div>
                   </div>
                 </div>
