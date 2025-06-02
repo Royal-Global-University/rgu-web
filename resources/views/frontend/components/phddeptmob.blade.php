48b8232a544db00568c0a116ad2627795440b73b
Royal-Global-University <ahussain1@rgu.ac>
2025-06-02 13:58:00 +0530

    new data

--- resources/views/frontend/components/phddeptmob.blade.php
+++ resources/views/frontend/components/phddeptmob.blade.php
@@ -172,7 +172,7 @@
         </div>
 
         <div class="col-lg-12 col-md-12 d-flex justify-content-center pt-2">
-            <a href="#" target="_blank"
+            <a href="doctoral-mathematics" target="_blank"
                 class="btn mobile-para1 w-100 d-flex align-items-center justify-content-center text-center"
                 style="background-color: #13223b; color: white; font-weight: bold; padding: 15px; border-radius: 5px; text-decoration: none; min-height: 60px;">
                 Mathematics
