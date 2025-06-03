48718a8532b88604b05ed75cec033cd52d1d7baa
Royal-Global-University <ahussain1@rgu.ac>
2025-06-03 18:38:52 +0530

    new pages

--- resources/views/frontend/discoverRGU/advisor-dr-amarjyoti-choudhury.blade.php
+++ resources/views/frontend/discoverRGU/advisor-dr-amarjyoti-choudhury.blade.php
@@ -2,12 +2,12 @@
 @section('content')
     <div class="mobile">
         @include('frontend/components/mobileheader')
-        <img style="padding-top: 90px; position: relative;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/advisor-leadership/new/new-advisory/mob-sri-amarjyoti.png"/>
+        <img style="padding-top: 90px; position: relative;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/advisor-leadership/new/new-advisory/mob-amarjyoti.png"/>
     </div>
 
     <div class="website">
         <!--head image Section-->
         @include('frontend/components/aheader')
-        <img src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/advisor-leadership/new/new-advisory/web-sri-amarjyoti.png"/>
+        <img src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/advisor-leadership/new/new-advisory/web-amarjyoti.png"/>
     </div>
 @endsection
