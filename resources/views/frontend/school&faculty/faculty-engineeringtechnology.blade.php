382a4d4070aab923b234fbb49bd171a143df4e32
Royal-Global-University <ahussain1@rgu.ac>
2025-06-04 23:05:22 +0530

    faculty link

--- resources/views/frontend/school&faculty/faculty-engineeringtechnology.blade.php
+++ resources/views/frontend/school&faculty/faculty-engineeringtechnology.blade.php
@@ -456,17 +456,22 @@
                 </div>
 
                 <div class="col-lg-3 mb-4">
-
-                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); ">
+                    <a href="dr-shwetambara-verma">
+                        <div class="card rounded d-flex flex-column"
+                            style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); ">
                             <div class="p-3">
-                              <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/rset/civil-engg/2.jpeg" class="img-fluid rounded"/>
+                                <img style="height: 290px; width: 100%;"
+                                    src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/rset/civil-engg/2.jpeg"
+                                    class="img-fluid rounded" />
                             </div>
                             <div class="card-body d-flex flex-column flex-grow-1">
-                              <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Prof. (Dr.) Shwetambara Verma</h5>
-                            <p class="para1" style="color: #27467A; line-height: 1.3;">Professor, <br> The Assam Royal Global University</p>
-
+                                <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Prof. (Dr.) Shwetambara Verma</h5>
+                                <p class="para1" style="color: #27467A; line-height: 1.3;">Professor, <br> The Assam Royal Global University</p>
+                                <!--profile page-->
+                                <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                             </div>
                         </div>
+                    </a>
                 </div>
 
                 <div class="col-lg-3 mb-4">
@@ -1058,7 +1063,7 @@
 
                 <div class="col-lg-3 mb-4">
 
-                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 510px;">
+                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 550px;">
                             <div class="p-3">
                               <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/rset/civil-engg/1.jpg" class="img-fluid rounded"/>
                             </div>
@@ -1071,22 +1076,26 @@
                 </div>
 
                 <div class="col-lg-3 mb-4">
-
-                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 510px;">
+                    <a href="dr-shwetambara-verma">
+                        <div class="card rounded d-flex flex-column"
+                            style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 550px;">
                             <div class="p-3">
-                              <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/rset/civil-engg/2.jpeg" class="img-fluid rounded"/>
+                                <img style="height: 290px; width: 100%;"
+                                    src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/rset/civil-engg/2.jpeg"
+                                    class="img-fluid rounded" />
                             </div>
                             <div class="card-body d-flex flex-column flex-grow-1">
-                              <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Prof. (Dr.) Shwetambara Verma</h5>
-                            <p class="para1" style="color: #27467A; line-height: 1.3;">Professor, <br> The Assam Royal Global University</p>
-
+                                <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Prof. (Dr.) Shwetambara Verma</h5>
+                                <p class="para1" style="color: #27467A; line-height: 1.3;">Professor, <br> The Assam Royal Global University</p>
+                                <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                             </div>
                         </div>
+                    </a>
                 </div>
 
                 <div class="col-lg-3 mb-4">
 
-                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 510px;">
+                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 550px;">
                             <div class="p-3">
                               <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/rset/civil-engg/3.jpg" class="img-fluid rounded"/>
                             </div>
@@ -1100,7 +1109,7 @@
 
                  <div class="col-lg-3 mb-4">
 
-                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 510px;">
+                        <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 550px;">
                             <div class="p-3">
                               <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/mobile-assets/all-faculty/rset/civil-engg/4.jpeg" class="img-fluid rounded"/>
                             </div>
