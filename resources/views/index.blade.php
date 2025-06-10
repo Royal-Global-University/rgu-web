9848ee7f87f291d7447849e59319b5567a2801d5
Royal-Global-University <ahussain1@rgu.ac>
2025-06-10 14:26:05 +0530

    video link look

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1479,7 +1479,7 @@
                         <div class="col-lg-5" style="display: flex; justify-content: center;">
                             <video autoplay muted playsinline
                                 style="width: 100%; height: auto; pointer-events: none; border: 4px solid #fff;">
-                                <source src="http://69.62.79.67/media/lookbook/lookbook.mp4"
+                                <source src="mobile-assets/lookbook/lookbook.mp4"
                                     type="video/mp4">
                                 Your browser does not support the video tag.
                             </video>
