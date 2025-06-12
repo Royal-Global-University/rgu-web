04a50c2249b6e82b02c59a7c2db762494001afb0
Royal-Global-University <ahussain1@rgu.ac>
2025-06-12 19:27:26 +0530

    schollar

--- resources/views/frontend/discoverRGU/vision-mission.blade.php
+++ resources/views/frontend/discoverRGU/vision-mission.blade.php
@@ -5,12 +5,12 @@
 @section('content')
     <div class="mobile">
         @include('frontend/components/mobileheader')
-        <img style="position: relative; padding-top: 80px;" src="http://69.62.79.67/media/vision-mission/mobile-vision-mission.png" alt="">
+        <img style="position: relative; padding-top: 80px;" src="mobile-assets/vision-mission/mobile-vision-mission.png" alt="">
     </div>
 
     <div class="website">
         @include('frontend/components/aheader')
-       <img src="http://69.62.79.67/media/vision-mission/web-vision-mission.png" alt="">
+       <img src="mobile-assets/vision-mission/web-vision-mission.png" alt="">
 
     </div>
 
