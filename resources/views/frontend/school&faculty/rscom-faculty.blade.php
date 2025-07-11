d4fef5b4ca69843d1da5d27119e8dd3da04acf76
Royal-Global-University <ahussain1@rgu.ac>
2025-07-11 12:35:29 +0530

    faculty

--- resources/views/frontend/school&faculty/rscom-faculty.blade.php
+++ resources/views/frontend/school&faculty/rscom-faculty.blade.php
@@ -55,7 +55,7 @@
                                 <p class="para1" style="color: #27467A; line-height: 1.3;">
                                     Assistant Professor & HOD Royal School of Communication & Media
                                 </p>
-                                <a href="faculty-champa-devi">
+                                <a href="faculty-florance-rabha">
                                     <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                                 </a>
                             </div>
@@ -291,7 +291,7 @@
                                 <p class="para1" style="color: #27467A; line-height: 1.3;">
                                     Assistant Professor & HOD Royal School of Communication & Media
                                 </p>
-                                <a href="faculty-champa-devi">
+                                <a href="faculty-florance-rabha">
                                     <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                                 </a>
                             </div>
