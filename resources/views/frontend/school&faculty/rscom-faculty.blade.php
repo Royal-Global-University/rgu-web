6afb1799456e8d3c1dc94feae3c7660c888c9cf4
Royal-Global-University <ahussain1@rgu.ac>
2025-07-11 13:01:21 +0530

    faculty

--- resources/views/frontend/school&faculty/rscom-faculty.blade.php
+++ resources/views/frontend/school&faculty/rscom-faculty.blade.php
@@ -27,7 +27,7 @@
                                 <p class="para1" style="color: #27467A; line-height: 1.3;">
                                     Dean i/c Royal School of Communication & Media
                                 </p>
-                                <a href="faculty-champa-devi">
+                                <a href="faculty-suresh-chandra">
                                     <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                                 </a>
                             </div>
@@ -263,7 +263,7 @@
                                 <p class="para1" style="color: #27467A; line-height: 1.3;">
                                     Dean i/c Royal School of Communication & Media
                                 </p>
-                                <a href="faculty-champa-devi">
+                                <a href="faculty-suresh-chandra">
                                     <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                                 </a>
                             </div>
