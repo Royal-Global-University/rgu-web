3d6cdb2eeece3bd2c411bce7d65342e853bef227
Royal-Global-University <ahussain1@rgu.ac>
2025-07-17 17:56:01 +0530

    facility

--- resources/views/frontend/facilities/facilities-auditorium.blade.php
+++ resources/views/frontend/facilities/facilities-auditorium.blade.php
@@ -2,17 +2,81 @@
 @section('content')
     <div class="mobile">
         @include('frontend/components/mobileheader')
-        <div style="padding-top: 90px; position: relative;">
-            <img src="mobile-assets/facilities-audi/mob-auditorium.png" alt="">
-        </div>
+        <section style="background-color: #fff8f0; padding: 30px 10px 20px 10px;">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+
+                        <h2 class="headd1 text-center pb-3" style="color: #264273; font-weight: 700; font-size: 32px;">
+                            Audito<span style="color: #FF9A1E; font-weight: 500;">rium</span></h2>
+
+                        <div class="txaa-slide-down-1">
+                            <div style="border: 2px solid #27467A; height: 320px; width: 100%;"
+                                class="kd-about-3-img img-cover fix kd-img-ani-1">
+                                <img class="rounded" decoding="async" src="mobile-assets/facilities-audi/headimg.png">
+                            </div>
+                        </div>
+
+                        <p class="para1 pt-3" style="color: #264273; text-align: justify;">
+                            Auditorium The Assam Royal Global University developed its latest auditorium with
+                            state-of-the-art
+                            technology as stipulated in the NEP 2020, designed with the special objective of generating
+                            digital experience in the teaching learning process. Inaugurated by Honourable Chief
+                            Minister of Meghalaya, Sri Conrad K. Sangma, it has a seating capacity of 400, and is
+                            equipped with an Advanced Projection System that delivers high-resolution images captured
+                            with stunning clarity. Complementing the visual excellence is a sophisticated surround sound
+                            setup that envelops the audience in rich, multidimensional audio, creating a truly immersive
+                            viewing environment.
+                        </p>
+
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+        </section>
+
+        <img src="mobile-assets/facilities-audi/btmimg-mob.png" alt="">
         </div>
 
         <div class="website">
-        <!--Start Header-->
-        @include('frontend/components/aheader')
-        <!--End Header-->
+            @include('frontend/components/aheader')
+        <!--head image Section-->
+        <section style="background-image: url(mobile-assets/hostel-life/bgg.svg); background-size: cover; padding: 50px 0px;">
+            <div class="container">
+                <div class="row align-items-center gx-5">
+                    <div class="col-lg-6">
+                        <div class="kd-about-3-img-wrap txaa-slide-down-1">
+                            <div style="border: 2px solid #27467A;" class="kd-about-3-img img-cover fix kd-img-ani-1">
+                                <img class="rounded" decoding="async" src="mobile-assets/facilities-audi/headimg.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
 
-        <img src="mobile-assets/facilities-audi/web-auditorium.png" alt="">
+                        <h2 class="headd1" style="color: #264273; font-weight: 700;">Audito<span
+                                style="color: #FF9A1E; font-weight: 500;">rium</span></h2>
+                        <p class="para1" style="color: #264273; font-size: 20px; text-align: justify;">
+                            Auditorium The Assam Royal Global University developed its latest auditorium with
+                            state-of-the-art
+                            technology as stipulated in the NEP 2020, designed with the special objective of generating
+                            digital experience in the teaching learning process. Inaugurated by Honourable Chief
+                            Minister of Meghalaya, Sri Conrad K. Sangma, it has a seating capacity of 400, and is
+                            equipped with an Advanced Projection System that delivers high-resolution images captured
+                            with stunning clarity. Complementing the visual excellence is a sophisticated surround sound
+                            setup that envelops the audience in rich, multidimensional audio, creating a truly immersive
+                            viewing environment.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--head image Section-->
+
+        <img src="mobile-assets/facilities-audi/btmimg.png" alt="">
 
         </div>
 
--- resources/views/frontend/facilities/facilities-seminar-hall.blade.php
+++ resources/views/frontend/facilities/facilities-seminar-hall.blade.php
@@ -2,17 +2,72 @@
 @section('content')
      <div class="mobile">
         @include('frontend/components/mobileheader')
-        <div style="padding-top: 90px; position: relative;">
-            <img src="mobile-assets/facilities-seminar-hall/mob.png" alt="">
-        </div>
+        <section style="background-color: #fff8f0; padding: 30px 10px 20px 10px;">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+
+                        <h2 class="headd1 text-center pb-3" style="color: #264273; font-weight: 700; font-size: 32px;">Seminar
+                            <span style="color: #FF9A1E; font-weight: 500;">Hall</span></h2>
+
+                        <div class="txaa-slide-down-1">
+                            <div style="border: 2px solid #27467A; height: 320px; width: 100%;"
+                                class="kd-about-3-img img-cover fix kd-img-ani-1">
+                                <img class="rounded" decoding="async" src="mobile-assets/facilities-seminar-hall/headimg.jpg"
+                                    alt="">
+                            </div>
+                        </div>
+
+                        <p class="para1 pt-3" style="color: #264273; text-align: justify;">
+                            RGU has several cutting-edge seminar halls and discussion rooms located throughout various
+                            academic buildings. It is a vibrant space pulsating with energy, where ideas ignite and
+                            discussions come to life.
+                        </p>
+
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+        </section>
+
+        <img src="mobile-assets/facilities-seminar-hall/btm-mob.png" alt="">
         </div>
 
         <div class="website">
-        <!--Start Header-->
-        @include('frontend/components/aheader')
-        <!--End Header-->
+            @include('frontend/components/aheader')
+        <!--head image Section-->
+        <section style="background-image: url(mobile-assets/hostel-life/bgg.svg); background-size: cover; padding: 50px 0px;">
+            <div class="container">
+                <div class="row align-items-center gx-5">
+                    <div class="col-lg-6">
+                        <div class="kd-about-3-img-wrap txaa-slide-down-1">
+                            <div style="border: 2px solid #27467A;" class="kd-about-3-img img-cover fix kd-img-ani-1">
+                                <img class="rounded" decoding="async" src="mobile-assets/facilities-seminar-hall/headimg.jpg"
+                                    alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
 
-        <img src="mobile-assets/facilities-seminar-hall/web.png" alt="">
+                        <h2 class="headd1" style="color: #264273; font-weight: 700;">Seminar <span
+                                style="color: #FF9A1E; font-weight: 500;">Halls</span></h2>
+                        <p class="para1" style="color: #264273; font-size: 20px; text-align: justify;">
+                            RGU has several cutting-edge seminar halls and discussion rooms located throughout various
+                            academic buildings. It is a vibrant space pulsating with energy, where ideas ignite and
+                            discussions come to life.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--head image Section-->
+
+        <img src="mobile-assets/facilities-seminar-hall/btm-web.png" alt="">
 
         </div>
 @endsection
