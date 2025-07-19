51efdfcd09801c443ea6522443bdcd3a0f94913b
Royal-Global-University <ahussain1@rgu.ac>
2025-07-19 12:52:12 +0530

    new faculty

--- resources/views/frontend/facultyprofile/rsaps/bornali-chetia.blade.php
+++ resources/views/frontend/facultyprofile/rsaps/bornali-chetia.blade.php
@@ -12,7 +12,7 @@
                         <div class="col-lg-6 text-center">
                             <div class="text-center">
                                 <img style="margin-top: 70px; width: 380px; border: 1px solid #27467A;"
-                                    src="mobile-assets/updated-faculty-img/dr-bornali-chetia-rsaps.png" alt="">
+                                    src="mobile-assets/updated-faculty-img/Dr-Bornali-Chetia-RSAPS.png" alt="">
                             </div>
                         </div>
 
@@ -249,7 +249,7 @@
 
                         <div class="text-center">
                             <img style="margin-top: 100px; width: 480px; border: 1px solid #27467A; margin-left: 45px;"
-                                src="mobile-assets/updated-faculty-img/dr-bornali-chetia-rsaps.png" alt="">
+                                src="mobile-assets/updated-faculty-img/Dr-Bornali-Chetia-RSAPS.png" alt="">
                         </div>
 
                     </div>
