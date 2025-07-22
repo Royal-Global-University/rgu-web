33abc2c82bc6a7c20bf20bd04cb050527b43101b
Royal-Global-University <ahussain1@rgu.ac>
2025-07-22 11:31:27 +0530

    new events

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -819,7 +819,7 @@
                                     Events at<span style="color: #FF9A1E; font-weight: 500;"> RGU</span></h2>
 
                                 <div id="slides" style="display: flex; transition: transform 0.7s ease;">
-                                    <img class="zoomable active" src="mobile-assets/events/17jul.jpg"
+                                    <img class="zoomable active" src="mobile-assets/events/22.jpg"
                                         style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
                                     <img class="zoomable" src="https://media.rgu.ac/index/e04.jpeg"
                                         style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
