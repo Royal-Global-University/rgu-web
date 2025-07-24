befb3ed9fd89e1dc1a40d000228ae44dd994ef0a
Royal-Global-University <ahussain1@rgu.ac>
2025-07-24 17:58:27 +0530

    updated faculty

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -1303,7 +1303,7 @@
                                                     <p class="m-0 pt-4 text-white" style="font-size: 18px;">Annual Report
                                                     </p>
                                                 </a>
-                                                <a href="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/Caution-Money-Refund-Application.pdf" >
+                                                <a href="mobile-assets/Alumni/Alumni Form.pdf" >
                                                     <p class="m-0 pt-4 text-white" style="font-size: 18px;">Caution Money Refund Application
                                                     </p>
                                                 </a>
