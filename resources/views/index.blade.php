6f15abf242ec6669bdbd348f88581930d861ac76
Royal-Global-University <ahussain1@rgu.ac>
2025-07-25 16:20:53 +0530

    dekhasharambh

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -853,8 +853,6 @@
                                         style="width:400px; height:530px; margin-right:20px; border: 4px solid #160c49; border-radius:10px;">
                                     <img src="mobile-assets/deekshara/new/6.jpeg"
                                         style="width:400px; height:530px; margin-right:20px; border: 4px solid #160c49; border-radius:10px;">
-                                    <img src="mobile-assets/deekshara/new/7.jpeg"
-                                        style="width:400px; height:530px; margin-right:20px; border: 4px solid #160c49; border-radius:10px;">
                                     <img src="mobile-assets/deekshara/new/8.jpeg"
                                         style="width:400px; height:530px; margin-right:20px; border: 4px solid #160c49; border-radius:10px;">
                                     <img src="mobile-assets/deekshara/new/9.jpeg"
