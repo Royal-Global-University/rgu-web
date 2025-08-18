4aaa0440bf78ece8a7583a9883ae2b7e927aebcf
Royal-Global-University <ahussain1@rgu.ac>
2025-08-18 14:33:52 +0530

    new

--- resources/views/frontend/school&faculty/faculty-engineeringtechnology.blade.php
+++ resources/views/frontend/school&faculty/faculty-engineeringtechnology.blade.php
@@ -1207,6 +1207,24 @@
                         <div class="card rounded d-flex flex-column"
                             style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 540px;">
                             <div class="p-3">
+                                <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Manash.jpg"
+                                    class="img-fluid rounded border" />
+                            </div>
+                            <div class="card-body d-flex flex-column flex-grow-1">
+                                <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Dr. Manash Protim Boruah</h5>
+                                <p class="para1" style="color: #27467A; line-height: 1.3;">
+                                    Assistant Professor <br>
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-4">
+
+                        <div class="card rounded d-flex flex-column"
+                            style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 540px;">
+                            <div class="p-3">
                                 <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Smrity.PNG"
                                     class="img-fluid rounded border" />
                             </div>
