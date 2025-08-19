3494bc7984da5e240924d38d1d3ea55e61ae2575
Royal-Global-University <ahussain1@rgu.ac>
2025-08-19 10:37:48 +0530

    syllabus

--- resources/views/frontend/school&faculty/faculty-applied-purescience.blade.php
+++ resources/views/frontend/school&faculty/faculty-applied-purescience.blade.php
@@ -332,23 +332,6 @@
                 <div class="card rounded d-flex flex-column"
                   style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); ">
                   <div class="p-3">
-                    <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Dr-Sayak-Sengupta.png"
-                      class="img-fluid rounded border" />
-                  </div>
-                  <div class="card-body d-flex flex-column flex-grow-1">
-                    <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Dr. Sayak Sengupta</h5>
-                    <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
-                    <a href="faculty-sayak-sengupta">
-                        <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-3 mb-4">
-                <div class="card rounded d-flex flex-column"
-                  style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); ">
-                  <div class="p-3">
                     <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Imo-Mani-Singha-RSAPS.png"
                       class="img-fluid rounded border" />
                   </div>
@@ -714,23 +697,6 @@
                     <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Dr. Nitu Borgohain</h5>
                     <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
                     <a href="faculty-dr-nitu-borgohain">
-                        <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-3 mb-4">
-                <div class="card rounded d-flex flex-column"
-                  style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); ">
-                  <div class="p-3">
-                    <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Dr-Sanchita-Roy-RSAPS.png"
-                      class="img-fluid rounded border" />
-                  </div>
-                  <div class="card-body d-flex flex-column flex-grow-1">
-                    <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Dr. Sanchita Roy</h5>
-                    <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
-                    <a href="faculty-dr-sanchita-roy">
                         <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                     </a>
                   </div>
@@ -1107,23 +1073,6 @@
                 <div class="card rounded d-flex flex-column"
                   style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 530px;">
                   <div class="p-3">
-                    <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Dr-Sayak-Sengupta.png"
-                      class="img-fluid rounded border" />
-                  </div>
-                  <div class="card-body d-flex flex-column flex-grow-1">
-                    <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Dr. Sayak Sengupta</h5>
-                    <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
-                    <a href="faculty-sayak-sengupta">
-                        <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-3 mb-4">
-                <div class="card rounded d-flex flex-column"
-                  style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 530px;">
-                  <div class="p-3">
                     <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Imo-Mani-Singha-RSAPS.png"
                       class="img-fluid rounded border" />
                   </div>
@@ -1490,23 +1439,6 @@
                     <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Dr. Nitu Borgohain</h5>
                     <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
                     <a href="faculty-dr-nitu-borgohain">
-                        <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-3 mb-4">
-                <div class="card rounded d-flex flex-column"
-                  style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 530px;">
-                  <div class="p-3">
-                    <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/Dr-Sanchita-Roy-RSAPS.png"
-                      class="img-fluid rounded border" />
-                  </div>
-                  <div class="card-body d-flex flex-column flex-grow-1">
-                    <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Dr. Sanchita Roy</h5>
-                    <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
-                    <a href="faculty-dr-sanchita-roy">
                         <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
                     </a>
                   </div>
