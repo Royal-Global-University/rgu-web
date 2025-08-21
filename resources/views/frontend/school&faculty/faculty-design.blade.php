1f435a0ddd200f169c7604030ae53c581ef5130b
Royal-Global-University <ahussain1@rgu.ac>
2025-08-21 15:54:13 +0530

    new faculty

--- resources/views/frontend/school&faculty/faculty-design.blade.php
+++ resources/views/frontend/school&faculty/faculty-design.blade.php
@@ -63,6 +63,22 @@
 
                     <div class="col-lg-3 mb-4">
                         <div class="card rounded d-flex flex-column"
+                            style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 500px;">
+                            <div class="p-3">
+                                <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/no-pic.png"
+                                    class="img-fluid rounded border" />
+                            </div>
+                            <div class="card-body d-flex flex-column flex-grow-1">
+                                <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Prof. Tikendra Kumar Sahu</h5>
+                                <p class="para1" style="color: #27467A; line-height: 1.3;">
+                                    Professor
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-4">
+                        <div class="card rounded d-flex flex-column"
                             style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); ">
                             <div class="p-3">
                                 <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/no-pic.png"
@@ -812,7 +828,21 @@
 
                 <div class="row" style="display: flex; align-items: center; justify-content: center;">
 
-
+                    <div class="col-lg-3 mb-4">
+                        <div class="card rounded d-flex flex-column"
+                            style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 560px;">
+                            <div class="p-3">
+                                <img style="height: 310px; width: 100%;" src="mobile-assets/updated-faculty-img/no-pic.png"
+                                    class="img-fluid rounded border" />
+                            </div>
+                            <div class="card-body d-flex flex-column flex-grow-1">
+                                <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Prof. Tikendra Kumar Sahu</h5>
+                                <p class="para1" style="color: #27467A; line-height: 1.3;">
+                                    Professor
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col-lg-3 mb-4">
 
                         <div class="card rounded d-flex flex-column"
