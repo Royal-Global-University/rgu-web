54dc1d34feccba2bffbcf269d133e3f67d490e51
Royal-Global-University <ahussain1@rgu.ac>
2025-09-07 21:53:52 +0530

    new video

--- resources/views/frontend/bhupen-video.blade.php
+++ resources/views/frontend/bhupen-video.blade.php
@@ -34,7 +34,7 @@
                 // Video Player Logic
                 const initVideoPlayer = () => {
                     const videos = [
-                        { src: "/bhupen-hazarika/vid1.mp4", title: "" },
+                        { src: "bhupen-hazarika/vid1.mp4", title: "" },
                         // Add other videos here if needed
                     ];
 
