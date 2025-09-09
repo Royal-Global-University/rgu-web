b3bc2019972a257feaa7a35e189c9c978111a34d
Royal-Global-University <ahussain1@rgu.ac>
2025-09-09 18:18:33 +0530

    new details

--- resources/views/frontend/school&faculty/faculty-bio-sciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-bio-sciences.blade.php
@@ -495,6 +495,9 @@
                                 <p class="para1" style="color: #27467A; line-height: 1.3;">
                                     Assistant Professor
                                 </p>
+                                <a href="faculty-nitesh-priyadarshi">
+                                    <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
+                                </a>
                             </div>
 
                         </div>
@@ -1052,6 +1055,9 @@
                                 <p class="para1" style="color: #27467A; line-height: 1.3;">
                                     Assistant Professor
                                 </p>
+                                <a href="faculty-nitesh-priyadarshi">
+                                    <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View Profile</p>
+                                </a>
                             </div>
 
                         </div>
