d704b1a0adc322965cb01097a92a5c5c1ebab1cc
Royal-Global-University <ahussain1@rgu.ac>
2025-09-24 16:45:01 +0530

    new

--- resources/views/frontend/school&faculty/rscom-faculty.blade.php
+++ resources/views/frontend/school&faculty/rscom-faculty.blade.php
@@ -136,6 +136,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Florence-Handique-Rabha.png"
+                                    alt="Profile image ">
+                            </div>
+                            <h3 class="card-name">Dr. Florence Handique Rabha</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            <a href="https://www.rgu.ac/faculty-florance-rabha" class="profile-link">View Profile</a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Sharmista-Pradhan-RSCOM.png"
                                     alt="Profile image ">
                             </div>
@@ -177,18 +189,6 @@
                             <h3 class="card-name">Dr. Esha Chakraborty</h3>
                             <p class="card-designation">Assistant Professor</p>
                             <a href="https://www.rgu.ac/faculty-esha-chakraborty" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Florence-Handique-Rabha.png"
-                                    alt="Profile image ">
-                            </div>
-                            <h3 class="card-name">Dr. Florence Handique Rabha</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-florance-rabha" class="profile-link">View Profile</a>
                         </div>
                     </div>
 
