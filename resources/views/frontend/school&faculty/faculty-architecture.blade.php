1f866d958ac44cb62969db65f0a849cae0c1c2b0
Royal-Global-University <ahussain1@rgu.ac>
2025-10-22 17:13:57 +0530

    new

--- resources/views/frontend/school&faculty/faculty-architecture.blade.php
+++ resources/views/frontend/school&faculty/faculty-architecture.blade.php
@@ -106,27 +106,6 @@
                             style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 480px;">
                             <div class="p-3">
                                 <img style="height: 300px; width: 100%;"
-                                    src="mobile-assets/updated-faculty-img/Barnali-Chakrabarty-RSA.png"
-                                    class="img-fluid rounded border" />
-                            </div>
-                            <div class="card-body d-flex flex-column flex-grow-1">
-                                <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Ar. Barnali
-                                    Chakrabarty
-                                </h5>
-                                <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
-                                <a href="faculty-barnali-chakraborty">
-                                    <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View
-                                        Profile</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 mb-4">
-                        <div class="card rounded d-flex flex-column"
-                            style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 480px;">
-                            <div class="p-3">
-                                <img style="height: 300px; width: 100%;"
                                     src="mobile-assets/updated-faculty-img/Barsha-Kunda-RSA.png"
                                     class="img-fluid rounded border" />
                             </div>
@@ -458,27 +437,6 @@
                                     Borah</h5>
                                 <p class="para1" style="color: #27467A; line-height: 1.3;">Associate Professor</p>
                                 <a href="faculty-kasturi-borah">
-                                    <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View
-                                        Profile</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 mb-4">
-                        <div class="card rounded d-flex flex-column"
-                            style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 480px;">
-                            <div class="p-3">
-                                <img style="height: 300px; width: 100%;"
-                                    src="mobile-assets/updated-faculty-img/Barnali-Chakrabarty-RSA.png"
-                                    class="img-fluid rounded border" />
-                            </div>
-                            <div class="card-body d-flex flex-column flex-grow-1">
-                                <h5 style="font-weight: 600; color: #27467A;" class="card-title headd2 pb-2">Ar. Barnali
-                                    Chakrabarty
-                                </h5>
-                                <p class="para1" style="color: #27467A; line-height: 1.3;">Assistant Professor</p>
-                                <a href="faculty-barnali-chakraborty">
                                     <p class="card-text para1 half-underline mt-auto fw-bold" style="color: #FF9A1E;">View
                                         Profile</p>
                                 </a>
