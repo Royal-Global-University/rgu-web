e4d86648ba9ef4855662acbe843d9f9d4205c59b
Royal-Global-University <ahussain1@rgu.ac>
2025-10-23 12:56:23 +0530

    new

--- resources/views/frontend/school&faculty/faculty-pharmacy.blade.php
+++ resources/views/frontend/school&faculty/faculty-pharmacy.blade.php
@@ -216,6 +216,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Tanniru.jpg"
+                                    alt="Profile image of">
+                            </div>
+                            <h3 class="card-name">Dr. Tanniru Rajeswari</h3>
+                            <p class="card-designation">Associate Professor</p>
+                            {{-- <a href="https://www.rgu.ac/faculty-sudarshana" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Anju-Das-RSP.png"
                                     alt="Profile image of">
                             </div>
