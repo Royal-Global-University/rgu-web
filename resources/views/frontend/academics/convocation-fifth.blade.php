0d6c99a7d6e899aa04f8ca38bb0f31ae7238e394
Royal-Global-University <ahussain1@rgu.ac>
2025-10-28 10:33:19 +0530

    new

--- resources/views/frontend/academics/convocation-fifth.blade.php
+++ resources/views/frontend/academics/convocation-fifth.blade.php
@@ -12,5 +12,5 @@
     <img src="/mobile-assets/convo-slider/convo.gif"/>
     <img src="/mobile-assets/convo-slider/1st-convo.jpg"/>
     <img src="/mobile-assets/convo-slider/honoris-causa-1.jpg"/>
-    <img src="/mobile-assets/convo-slider/list.jpg"/>
+    <img src="/mobile-assets/convo-slider/last.jpg"/>
 @endsection
