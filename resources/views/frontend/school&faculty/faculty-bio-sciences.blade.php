a4f4df56f307ffcc544735be726739e563f8c046
Royal-Global-University <ahussain1@rgu.ac>
2025-11-14 14:48:25 +0530

    new

--- resources/views/frontend/school&faculty/faculty-bio-sciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-bio-sciences.blade.php
@@ -718,18 +718,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Barnali-Chakrabarty-RSA.png"
-                                    alt="Profile image of Ar. Barnali Chakrabarty">
-                            </div>
-                            <h3 class="card-name">Ar. Barnali Chakrabarty</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-barnali-chakraborty" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="mobile-assets/updated-faculty-img/Ms-Jahnabi-Mahanta-RSBAS.png"
                                     alt="Profile image of Jahnabi">
                             </div>
@@ -915,18 +903,6 @@
                             <h3 class="card-name">Romeet Saha</h3>
                             <p class="card-designation">Assistant Professor</p>
                             <a href="https://www.rgu.ac/faculty-romeet-saha" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src="https://media.rgu.ac/iks/Dr.SiddhantMedhi.jpg"
-                                    alt="Profile image of Siddhant Medhi">
-                            </div>
-                            <h3 class="card-name">Dr. Siddhant Medhi</h3>
-                            <p class="card-designation">Faculty Member, IKS Cell</p>
-                            <a href="https://www.rgu.ac/faculty-siddhant-medhi" class="profile-link">View Profile</a>
                         </div>
                     </div>
 
