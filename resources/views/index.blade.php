59467d14a4291c284a2ea5c0c8418137bb0401ad
Royal-Global-University <ahussain1@rgu.ac>
2025-11-27 15:00:20 +0530

    new

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,8 @@
     <section class="mobile">
 
         <!-- Popup -->
-        {{-- <div id="popup-mob" class="popup-overlay">
-            <a target="_blank" class="text-center"><img src="{{ asset('/mobile-assets/convo-slider/popup.jpg') }}"
+        <div id="popup-mob" class="popup-overlay">
+            <a target="_blank" class="text-center"><img src="{{ asset('/mobile-assets/popup/phd.jpeg') }}"
                     style="width: 400px; height: 400px;" class="popup-image" alt="Popup Image"></a>
         </div>
 
@@ -24,7 +24,7 @@
                     }
                 });
             });
-        </script> --}}
+        </script>
 
         <!-- Floating WhatsApp Button -->
         <a href="https://wa.me/917099028518" class="whatsapp-float" target="_blank">
@@ -618,7 +618,7 @@
 
     <section class="website">
         <!-- Popup -->
-        {{-- <div id="popup-web" class="popup-overlay"
+        <div id="popup-web" class="popup-overlay"
             style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0,0.6); display: flex; justify-content: center; align-items: center; z-index: 9999;">
 
             <div style="position: relative; display: inline-block;">
@@ -634,7 +634,7 @@
 
                 <!-- Popup Image -->
                 <a>
-                    <img src="{{ asset('/mobile-assets/convo-slider/popup.jpg') }}"
+                    <img src="{{ asset('/mobile-assets/popup/phd.jpeg') }}"
                         style="width: 700px; height: 550px; border-radius: 8px;" class="popup-image" alt="Popup Image">
                 </a>
             </div>
@@ -657,7 +657,7 @@
                     popup.style.display = 'none';
                 });
             });
-        </script> --}}
+        </script>
 
         <!-- floating button  -->
         {{-- <div>
@@ -1080,12 +1080,8 @@
                                 </h2>
 
                                 <div id="slides" style="display: flex; transition: transform 0.7s ease;">
-                                    {{-- <img class="zoomable" src="/mobile-assets/events/25-nov.jpg"
+                                    <img class="zoomable" src="/mobile-assets/events/phd27.jpeg"
                                         style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
-                                    <img class="zoomable" src="/mobile-assets/events/251.jpeg"
-                                        style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;"> --}}
-                                    {{-- <img class="zoomable" src="mobile-assets/events/nov172.jpeg"
-                                        style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;"> --}}
                                     <img class="zoomable" src="mobile-assets/events/techer.jpeg"
                                         style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
                                     <img class="zoomable" src="mobile-assets/events/spot12.png"
