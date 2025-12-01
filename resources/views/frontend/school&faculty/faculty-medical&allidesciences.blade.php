73aacf4eaf89b8bbab73d2ac3c5e152cb0a65c2d
Royal-Global-University <ahussain1@rgu.ac>
2025-12-01 18:01:32 +0530

    add faculty

--- resources/views/frontend/school&faculty/faculty-medical&allidesciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-medical&allidesciences.blade.php
@@ -829,6 +829,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Aniruddha.jpg"
+                                    alt="Profile image of Mr. Aniruddha Sen">
+                            </div>
+                            <h3 class="card-name">Mr. Aniruddha Sen</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            <!-- <a href="" class="profile-link">View Profile</a> -->
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/p1.png"
                                     alt="Profile image of Ms. Farnaz Ahmed">
                             </div>
