ae8c8bda4cc43a0718945ea40646b1bdb7ebd9c6
Royal-Global-University <ahussain1@rgu.ac>
2025-12-03 10:27:20 +0530

    new

--- resources/views/frontend/conference/frontiers.blade.php
+++ resources/views/frontend/conference/frontiers.blade.php
@@ -798,7 +798,7 @@
                                 <div class="card-image">
                                     <img src="mobile-assets/blog/cosmology/profiles/Picture8.png" alt="Profile image">
                                 </div>
-                                <h3 class="card-name">Prof. Madhurjya P. Borah</h3>
+                                <h3 class="card-name">Prof. Madhurjya P. Bora</h3>
                                 <p class="card-designation">Gauhati University</p>
 
                             </div>
@@ -1632,7 +1632,7 @@
                                 <div class="card-image">
                                     <img src="mobile-assets/blog/cosmology/profiles/Picture8.png" alt="Profile image">
                                 </div>
-                                <h3 class="card-name">Prof. Madhurjya P. Borah</h3>
+                                <h3 class="card-name">Prof. Madhurjya P. Bora</h3>
                                 <p class="card-designation">Gauhati University</p>
 
                             </div>
