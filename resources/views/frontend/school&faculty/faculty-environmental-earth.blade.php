86957c6ec5a794a9effc9860cd117a30ed2cf5f5
Royal-Global-University <ahussain1@rgu.ac>
2025-12-05 12:45:04 +0530

    new faculty

--- resources/views/frontend/school&faculty/faculty-environmental-earth.blade.php
+++ resources/views/frontend/school&faculty/faculty-environmental-earth.blade.php
@@ -146,6 +146,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Arghadeep.jpeg"
+                                    alt="r. Arghadeep Bose">
+                            </div>
+                            <h3 class="card-name">Dr. Arghadeep Bose</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="https://www.rgu.ac/faculty-annesha-borah" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Dr-Deepanjali-Reang-RSHSS.png"
                                     alt="Profile image ">
                             </div>
