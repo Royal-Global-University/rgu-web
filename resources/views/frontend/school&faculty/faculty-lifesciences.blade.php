e742e2aa9c885ec540fa6841706105007d92584d
Royal-Global-University <ahussain1@rgu.ac>
2025-12-08 17:15:39 +0530

    zoology

--- resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
@@ -280,18 +280,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Utpal.jpeg"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Prof. (Dr.) Utpal Dutta</h3>
-                            <p class="card-designation">Professor, Zoology and Director, Finishing School</p>
-                            <a href="https://www.rgu.ac/faculty-dr-utpal-dutta" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/all-faculty/rslsc/annukumari.jpeg"
                                     alt="Profile image">
                             </div>
