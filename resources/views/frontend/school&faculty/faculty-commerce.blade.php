9a0bf91556dda034de469f23af14f4f570ebade0
Royal-Global-University <ahussain1@rgu.ac>
2025-12-09 15:06:01 +0530

    new

--- resources/views/frontend/school&faculty/faculty-commerce.blade.php
+++ resources/views/frontend/school&faculty/faculty-commerce.blade.php
@@ -178,22 +178,22 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="mobile-assets/updated-faculty-img/Dr-Arpee-Saikia-RSB.png" alt="Profile image ">
+                                <img src="mobile-assets/all-faculty/rsc/3.jpg" alt="Profile image ">
                             </div>
-                            <h3 class="card-name">Dr. Arpee Saikia</h3>
-                            <p class="card-designation">Associate Professor</p>
-                            <a href="https://www.rgu.ac/faculty-arpee-saikia" class="profile-link">View Profile</a>
+                            <h3 class="card-name">Dr. Aruna Deb Rroy</h3>
+                            <p class="card-designation">Associate Professor & HoD</p>
+                            <a href="https://www.rgu.ac/faculty-dr-aruna-dev-roy" class="profile-link">View Profile</a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="mobile-assets/all-faculty/rsc/3.jpg" alt="Profile image ">
+                                <img src="mobile-assets/updated-faculty-img/Dr-Arpee-Saikia-RSB.png" alt="Profile image ">
                             </div>
-                            <h3 class="card-name">Dr. Aruna Deb Rroy</h3>
+                            <h3 class="card-name">Dr. Arpee Saikia</h3>
                             <p class="card-designation">Associate Professor</p>
-                            <a href="https://www.rgu.ac/faculty-dr-aruna-dev-roy" class="profile-link">View Profile</a>
+                            <a href="https://www.rgu.ac/faculty-arpee-saikia" class="profile-link">View Profile</a>
                         </div>
                     </div>
 
@@ -232,7 +232,7 @@
                         </div>
                     </div>
 
-                     <div class="col-lg-3 col-md-6 mb-4">
+                    <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
                                 <img src="mobile-assets/updated-faculty-img/Neha-Tiwari.png"
