a661e0ac5b89688611c4291d47b7689f76549551
Royal-Global-University <ahussain1@rgu.ac>
2025-12-22 18:02:31 +0530

    new

--- resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
@@ -350,6 +350,30 @@
                         </div>
                     </div>
 
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="mobile-assets/updated-faculty-img/Diksha.jpeg"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Diksha Dutta</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            <!-- <a href="" class="profile-link">View Profile</a> -->
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="mobile-assets/updated-faculty-img/Mandira.jpeg"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Mandira Basumatary</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            <!-- <a href="" class="profile-link">View Profile</a> -->
+                        </div>
+                    </div>
+
 
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
