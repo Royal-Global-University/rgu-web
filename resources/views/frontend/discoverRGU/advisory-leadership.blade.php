6c17df5895922ed3cb3134366d41740ef17ada11
Royal-Global-University <ahussain1@rgu.ac>
2025-12-26 13:02:41 +0530

    new

--- resources/views/frontend/discoverRGU/advisory-leadership.blade.php
+++ resources/views/frontend/discoverRGU/advisory-leadership.blade.php
@@ -4,8 +4,8 @@
 
     <div class="mobile">
         @include('frontend/components/mobileheader')
-      <h2 class="headd1 fw-bold text-center kd-title-ani kd-split-text pt-5" style="color: #27467A; font-weight: 900;">
-        Advisory <br><span style="color: #FF9A1E; font-weight: 500; padding-top: 80px; position: relative;"> Leadership</span></h2>
+      <h2 class="headd1 fw-bold text-center kd-title-ani kd-split-text" style="color: #27467A; font-weight: 900; padding-top: 100px;">
+        Advisory <br><span style="color: #FF9A1E; font-weight: 500;"> Leadership</span></h2>
     </div>
 
     <div class="website">
