70aeceda6fd28b97ace1945be245b5db306d8342
Royal-Global-University <ahussain1@rgu.ac>
2026-01-02 18:03:08 +0530

    today all new

--- resources/views/frontend/school&faculty/faculty-agriculture.blade.php
+++ resources/views/frontend/school&faculty/faculty-agriculture.blade.php
@@ -250,18 +250,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="mobile-assets/updated-faculty-img/raghuvar-tiwary-botany.jpeg"
-                                    alt="Profile image of Dr. Raghuvar Tiwary">
-                            </div>
-                            <h3 class="card-name">Dr. Raghuvar Tiwary</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <!-- <a href="" class="profile-link">View Profile</a> -->
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="mobile-assets/all-faculty/rslsc/Sangeeta.jpeg"
                                     alt="Profile image of Ms. Sangeeta Biswas">
                             </div>
