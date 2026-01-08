eb191ac7c4ddd2adf60bfa2fbeb57329b70ed6a3
Royal-Global-University <ahussain1@rgu.ac>
2026-01-08 15:21:52 +0530

    add faculty

--- resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
+++ resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
@@ -646,6 +646,19 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="mobile-assets/updated-faculty-img/Akash.jpeg"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Akash Singh Thakur</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="https://www.rgu.ac/faculty-dhiraj-kumar-borkotoky" class="profile-link">View
+                                Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Baba-Chandra-Singha.jpg"
                                     alt="Profile image">
                             </div>
