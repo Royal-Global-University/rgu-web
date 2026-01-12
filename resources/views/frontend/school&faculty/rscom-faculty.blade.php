5bfb53ea450fbd6b520b36f6cc191ddde2f2e827
Royal-Global-University <ahussain1@rgu.ac>
2026-01-12 17:19:21 +0530

    remove

--- resources/views/frontend/school&faculty/rscom-faculty.blade.php
+++ resources/views/frontend/school&faculty/rscom-faculty.blade.php
@@ -220,18 +220,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Subhajit.jpeg"
-                                    alt="Profile image ">
-                            </div>
-                            <h3 class="card-name">Dr. Subhajit Paul</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-subhajit-paul" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/suresh.jpg"
                                     alt="Profile image ">
                             </div>
