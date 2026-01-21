964798b4d8039d02797ba4807393fad2a7f9e81d
Royal-Global-University <ahussain1@rgu.ac>
2026-01-21 15:09:15 +0530

    research

--- resources/views/frontend/research/research-r&d-team.blade.php
+++ resources/views/frontend/research/research-r&d-team.blade.php
@@ -12,23 +12,6 @@
 
                         <div class="row" style="display: flex; justify-content: center; align-items: center;">
 
-                            <div class="col-lg-3 mb-4">
-
-                                    <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 450px;">
-                                        <div class="p-3">
-                                          <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/r-team/Professor-Rama-Ranjan-Bhattacharjee.jpeg" class="img-fluid rounded"/>
-                                        </div>
-                                        <div class="card-body d-flex flex-column flex-grow-1">
-                                          <h5 style="font-weight: 700; color: #27467A;" class="card-title headd2 pb-2">Professor Rama Ranjan Bhattacharjee, Dean R & D</h5>
-                                        <p class="para1" style="color: #27467A; line-height: 1.3;">Chairman</p>
-                                        </div>
-                                    </div>
-                            </div>
-
-                            </div>
-
-                            <hr>
-
                             <div class="row">
 
                                 <div class="col-lg-3 mb-4">
@@ -217,25 +200,6 @@
                 Research and <span style="color: #FF9A1E; font-weight: 500;">Development Team</span></h2>
 
                 <div style="padding: 20px 80px;">
-
-                    <div class="row" style="display: flex; justify-content: center; align-items: center;">
-
-                    <div class="col-lg-3 mb-4">
-
-                            <div class="card rounded d-flex flex-column" style="background-color: #fff; border: none; box-shadow: -1px 3px 5px 0px rgba(0,0,0,0.25); height: 450px;">
-                                <div class="p-3">
-                                  <img style="height: 290px; width: 100%;" src="https://bucket-0534zd.s3.ap-south-1.amazonaws.com/r-team/Professor-Rama-Ranjan-Bhattacharjee.jpeg" class="img-fluid rounded"/>
-                                </div>
-                                <div class="card-body d-flex flex-column flex-grow-1">
-                                  <h5 style="font-weight: 700; color: #27467A;" class="card-title headd2 pb-2">Professor Rama Ranjan Bhattacharjee, Dean R&D</h5>
-                                <p class="para1" style="color: #27467A; line-height: 1.3;">Chairman</p>
-                                </div>
-                            </div>
-                    </div>
-
-                    </div>
-
-                    <hr>
 
                     <div class="row">
 
