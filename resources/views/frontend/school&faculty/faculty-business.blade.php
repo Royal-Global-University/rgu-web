31711a3de460a829fbdae52f7e6bdf374c199489
Royal-Global-University <ahussain1@rgu.ac>
2026-02-06 12:11:59 +0530

    dr

--- resources/views/frontend/school&faculty/faculty-business.blade.php
+++ resources/views/frontend/school&faculty/faculty-business.blade.php
@@ -525,18 +525,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Neha-Tiwari.png"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Dr. </h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-neha-tiwari" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Dr-Niva-Kalita-RSC.png"
                                     alt="Profile image">
                             </div>
