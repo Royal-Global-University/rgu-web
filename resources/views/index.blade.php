b600b69cf7a7ed7c6a4df649736a999a6d6beeff
Royal-Global-University <ahussain1@rgu.ac>
2026-02-06 10:29:40 +0530

    events

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -955,8 +955,6 @@
                                 </h2>
 
                                 <div id="slides" style="display: flex; transition: transform 0.7s ease;">
-                                    <img class="zoomable" src="mobile-assets/events/football.jpeg"
-                                        style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
                                     <img class="zoomable" src="mobile-assets/events/techer.jpeg"
                                         style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
                                     <img class="zoomable" src="mobile-assets/events/spot12.png"
