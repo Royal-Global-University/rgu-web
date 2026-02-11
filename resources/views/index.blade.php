d013f67a9d9653f836df8185668e1834f8eb3818
Royal-Global-University <ahussain1@rgu.ac>
2026-02-11 18:17:14 +0530

    news

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -958,11 +958,8 @@
                                     <img class="zoomable" src="mobile-assets/events/techer.jpeg"
                                         style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
 
-                                         <img class="zoomable" src="mobile-assets/events/feb-9.png"
+                                         <img class="zoomable" src="mobile-assets/events/feb11.jpeg"
                                         style="width:300px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
-
-                                    <img class="zoomable" src="mobile-assets/events/feb10.jpeg"
-                                        style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
                                     <img class="zoomable" src="mobile-assets/events/sep-15.jpeg"
                                         style="width:400px; height: 530px; margin-right: 20px; border: 2px solid #e68900; border-radius: 10px;">
                                     <img class="zoomable" src="mobile-assets/events/stu-3.jpeg"
