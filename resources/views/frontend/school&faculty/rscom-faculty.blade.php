727067fc4f0d69cdd9be11db5885f50eadc7ff70
Royal-Global-University <ahussain1@rgu.ac>
2026-02-16 17:17:59 +0530

    faculty profile

--- resources/views/frontend/school&faculty/rscom-faculty.blade.php
+++ resources/views/frontend/school&faculty/rscom-faculty.blade.php
@@ -113,28 +113,26 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/all-faculty/rsb/2.jpg" alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Prof. D.N. Singh</h3>
-                            <p class="card-designation">Professor & Registrar, Academics & I/C Dean, Royal School of
-                                Communication &
-                                Media</p>
-                            <a href="https://www.rgu.ac/Prof-dn-singh" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="https://media.rgu.ac/advisor-leadership/Prasanta.jpeg" alt="Profile image">
                             </div>
-                            <h3 class="card-name">Dr. Prasanta Jyoti Baruah</h3>
-                            <p class="card-designation">Distinguished Professor, RSCOM</p>
+                            <h3 class="card-name">Prof. Prasanta Jyoti Baruah</h3>
+                            <p class="card-designation">Distinguished Professor & Dean, RSCOM</p>
                             <a href="https://www.rgu.ac/advisor-prasanta-jyoti-baruah" class="profile-link">View Profile</a>
                         </div>
                     </div>
 
                     <!-- Professor -->
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="https://www.rgu.ac/mobile-assets/all-faculty/rsb/2.jpg" alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Prof. D.N. Singh</h3>
+                            <p class="card-designation">Professor & Registrar, Academics</p>
+                            <a href="https://www.rgu.ac/Prof-dn-singh" class="profile-link">View Profile</a>
+                        </div>
+                    </div>
 
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
