9343caa9833cca3d3f0c367c7a7b001e0b78f47f
Royal-Global-University <ahussain1@rgu.ac>
2026-03-02 13:11:25 +0530

    add faculty

--- resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
+++ resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
@@ -881,6 +881,18 @@
                 <!-- Social Work  -->
 
                 <div class="row justify-content-center">
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/victor-narzary.jpeg" alt="Profile image ">
+                            </div>
+                            <h3 class="card-name">Dr. Victor Narzary</h3>
+                            <p class="card-designation">Associate Professor</p>
+                            {{-- <a href="https://www.rgu.ac/faculty-priyanka-patowari" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
--- resources/views/frontend/school&faculty/faculty-law-administration.blade.php
+++ resources/views/frontend/school&faculty/faculty-law-administration.blade.php
@@ -201,6 +201,18 @@
                         </div>
                     </div>
 
+                     <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="mobile-assets/updated-faculty-img/jahnavi-b.jpeg"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Jahnavi Bhandari</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="https://www.rgu.ac/faculty-bhupali-saikia" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
