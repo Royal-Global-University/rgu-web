f63700bd8b971c5974e781df1b49bfb0cad7c65a
Royal-Global-University <ahussain1@rgu.ac>
2026-03-05 11:58:48 +0530

    new faculty

--- resources/views/frontend/school&faculty/faculty-pharmacy.blade.php
+++ resources/views/frontend/school&faculty/faculty-pharmacy.blade.php
@@ -310,6 +310,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="mobile-assets/updated-faculty-img/nayanika.jpeg"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Ms. Nayanika Neog</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="https://www.rgu.ac/himanta-saikia" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Neelakshi-Sharma-RSP.png"
                                     alt="Profile image">
                             </div>
