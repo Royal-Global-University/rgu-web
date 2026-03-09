587cba73c4fbabd779cca6a83b063b1145a4f476
Royal-Global-University <ahussain1@rgu.ac>
2026-03-09 12:48:48 +0530

    media corner

--- resources/views/frontend/school&faculty/faculty-allied-sciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-allied-sciences.blade.php
@@ -175,6 +175,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="mobile-assets/updated-faculty-img/ravina.jpeg"
+                                    alt="Profile image ">
+                            </div>
+                            <h3 class="card-name">Dr. Ravina Khumanlambam</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="https://www.rgu.ac/olivia-kakati" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Dr-Shraddha-Basu-RSBAS.png"
                                     alt="Profile image ">
                             </div>
