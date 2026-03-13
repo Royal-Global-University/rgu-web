249e73abbb666b0efafd964167790d9618b8eeb9
Royal-Global-University <ahussain1@rgu.ac>
2026-03-13 15:02:26 +0530

    faculty profile

--- resources/views/frontend/school&faculty/faculty-languages.blade.php
+++ resources/views/frontend/school&faculty/faculty-languages.blade.php
@@ -338,7 +338,7 @@
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Dr-Rani-Das.png"
                                     alt="Profile image ">
                             </div>
-                            <h3 class="card-name">Dr. Rani Das</h3>
+                            <h3 class="card-name">Dr. Rani Pathak Das</h3>
                             <p class="card-designation">Assistant Professor, Social Work and Public Relation Officer</p>
                             <!-- <a href="" class="profile-link">View Profile</a> -->
                         </div>
