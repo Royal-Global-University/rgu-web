a8ac27d919280216ef335df306a01e2e4ad91145
Royal-Global-University <ahussain1@rgu.ac>
2026-03-17 09:26:01 +0530

    faculty

--- resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
@@ -278,6 +278,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="https://www.rgu.ac/mobile-assets/all-faculty/rslsc/ParimalChandra.jpeg"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Parimal Chandra Ray</h3>
+                            <p class="card-designation">Assistant Professor & Coordinator</p>
+                            <a href="/faculty-parimal-chandra-ray" class="profile-link">View Profile</a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/all-faculty/rslsc/AbeshChakraborty.jpeg"
                                     alt="Profile image">
                             </div>
@@ -344,20 +356,6 @@
                             <a href="/faculty-mandira-basumatary" class="profile-link">View Profile</a>
                         </div>
                     </div>
-
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/all-faculty/rslsc/ParimalChandra.jpeg"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Dr. Parimal Chandra Ray</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="/faculty-parimal-chandra-ray" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
 
                 </div>
                 <!-- regular faculty  -->
