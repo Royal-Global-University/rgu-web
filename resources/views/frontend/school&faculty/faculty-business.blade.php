a2c2ce48153bd91a8a2b7b19942dbcc92cb9ebe4
Royal-Global-University <ahussain1@rgu.ac>
2026-03-18 09:31:33 +0530

    add faculty

--- resources/views/frontend/school&faculty/faculty-business.blade.php
+++ resources/views/frontend/school&faculty/faculty-business.blade.php
@@ -471,18 +471,6 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Debarchana.PNG"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Dr. Debarchana Baruah</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-debarchana-baruah" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
                     {{-- <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
--- resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
+++ resources/views/frontend/school&faculty/faculty-lifesciences.blade.php
@@ -423,18 +423,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Debarchana.PNG"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Dr. Debarchana Baruah</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-debarchana-baruah" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Dr-Tahera-Haque-Majumder-RSBAS.png"
                                     alt="Profile image">
                             </div>
@@ -722,22 +710,6 @@
                                 <p class="card-designation">Assistant Professor
                                 </p>
                                 <a href="/faculty-chandni-khaund" class="profile-link">View Profile</a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-md-6 mb-4">
-                            <div class="card">
-                                <div class="card-image">
-                                    <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Debarchana.PNG"
-                                        alt="Profile image">
-                                </div>
-                                <h3 class="card-name">Dr. Debarchana Baruah
-                                </h3>
-                                <p class="card-designation">Assistant Professor
-
-                                </p>
-                                <a href="https://www.rgu.ac/faculty-debarchana-baruah" class="profile-link">View
-                                    Profile</a>
                             </div>
                         </div>
 
