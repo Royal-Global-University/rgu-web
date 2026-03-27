8cdb03b255068827b03f4b41d88864bc95d3b2ee
Royal-Global-University <ahussain1@rgu.ac>
2026-03-27 13:05:13 +0530

    research

--- resources/views/frontend/test/preface.blade.php
+++ resources/views/frontend/test/preface.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <section class="pg-hero">
-        <div class="pg-hero-bg" style="background-image:url('/new-web/assets/img/discover-rgu-preface/img1.jpg');"></div>
+        <div class="pg-hero-bg" style="background-image:url('https://media.rgu.ac/new-web/img/discover-rgu-preface/img1.jpg');"></div>
 
         <div class="pg-hero-bg"></div>
         <div class="pg-hero-overlay"></div>
@@ -34,7 +34,7 @@
 
             <!-- LEFT IMAGE -->
             <div class="pf-img-wrap">
-                <img src="/new-web/assets/img/discover-rgu-preface/img1.jpg" alt="RGU Campus">
+                <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/img1.jpg" alt="RGU Campus">
             </div>
 
             <!-- RIGHT CONTENT -->
@@ -56,7 +56,7 @@
     </section> --}}
 
     <section>
-        <img style="width: 100%;" src="/new-web/assets/img/discover-rgu-preface/curve1.svg" alt="">
+        <img style="width: 100%;" src="https://media.rgu.ac/new-web/img/discover-rgu-preface/curve1.svg" alt="">
     </section>
 
     <section class="dg-sec">
@@ -66,28 +66,28 @@
             <div class="dg-col">
                 <a href="/vision-mission" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/vision-mission.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/vision-mission.svg" alt="">
                     </div>
                     <span>VISION AND MISSION</span>
                 </a>
 
                 <a href="/award" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/awards-honors.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/awards-honors.svg" alt="">
                     </div>
                     <span>AWARDS AND HONORS</span>
                 </a>
 
                 <a href="/social-outreach" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/philantrophy.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/philantrophy.svg" alt="">
                     </div>
                     <span>PHILANTHROPY</span>
                 </a>
 
                 <a href="/knowledge" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/knowledge-base.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/knowledge-base.svg" alt="">
                     </div>
                     <span>KNOWLEDGE BASE</span>
                 </a>
@@ -98,54 +98,54 @@
 
                 <a href="/recognition-accreditation" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/recignition.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/recignition.svg" alt="">
                     </div>
                     <span>RECOGNITION AND ACCREDITATION</span>
                 </a>
 
                 <a href="/leadership-chancellor" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/world-of.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/world-of.svg" alt="">
                     </div>
                     <span>WORLD OF OPPORTUNITIES</span>
                 </a>
 
                 <a href="/facilities" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/facilities.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/facilities.svg" alt="">
                     </div>
                     <span>FACILITIES</span>
                 </a>
 
                 <a href="/leadership" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/leadership-team.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/leadership-team.svg" alt="">
                     </div>
                     <span>LEADERSHIP TEAM</span>
                 </a>
 
                 <!-- IMAGE CARD -->
                 <a class="dg-card dg-image">
-                    <img src="/new-web/assets/img/discover-rgu-preface/img2.jpg" alt="">
+                    <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/img2.jpg" alt="">
                 </a>
 
                 <a href="/privacy&security" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/privacy.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/privacy.svg" alt="">
                     </div>
                     <span>PRIVACY AND DATA PROTECTION</span>
                 </a>
 
                 <a href="/scholarship&funding" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/scholarship.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/scholarship.svg" alt="">
                     </div>
                     <span>SCHOLARSHIP AND FUNDING</span>
                 </a>
 
                 <a href="/association-tieup" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/association-tieups.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/association-tieups.svg" alt="">
                     </div>
                     <span>TIE UPS AND ASSOCIATION</span>
                 </a>
@@ -156,28 +156,28 @@
             <div class="dg-col">
                 <a href="/strategic-location" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/stre-location.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/stre-location.svg" alt="">
                     </div>
                     <span>STRATEGIC LOCATION</span>
                 </a>
 
                 <a href="/jobs-vacancies" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/job-vacancy.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/job-vacancy.svg" alt="">
                     </div>
                     <span>JOBS AND VACANCY</span>
                 </a>
 
                 <a href="/research-publication-overview" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/research.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/research.svg" alt="">
                     </div>
                     <span>RESEARCH AND PUBLICATION</span>
                 </a>
 
                 <a href="" class="dg-card">
                     <div class="dg-icon">
-                        <img src="/new-web/assets/img/discover-rgu-preface/card-icons/alumni.svg" alt="">
+                        <img src="https://media.rgu.ac/new-web/img/discover-rgu-preface/card-icons/alumni.svg" alt="">
                     </div>
                     <span>ALUMNI</span>
                 </a>
@@ -187,7 +187,7 @@
     </section>
 
     <section>
-        <img style="width: 100%;" src="/new-web/assets/img/discover-rgu-preface/curve2.svg" alt="">
+        <img style="width: 100%;" src="https://media.rgu.ac/new-web/img/discover-rgu-preface/curve2.svg" alt="">
     </section>
 
 @endsection
