fde937456ea05c4b2c48413215ece64711967595
Royal-Global-University <ahussain1@rgu.ac>
2026-04-01 12:15:13 +0530

    phd

--- resources/views/frontend/school&faculty/faculty-agriculture.blade.php
+++ resources/views/frontend/school&faculty/faculty-agriculture.blade.php
@@ -135,22 +135,6 @@
                         </div>
                     </div>
 
-                    {{-- <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Mehjebin.PNG"
-                                    alt="Profile image of Dr. Mehjebin Rahman">
-                            </div>
-                            <h3 class="card-name">Dr. Mehjebin Rahman</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-Mehjabin-Rahman" class="profile-link">View Profile</a>
-                        </div>
-                    </div> --}}
-
-
-
-                    <div class="col-lg-1 col-md-6 mb-4"></div>
-
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
