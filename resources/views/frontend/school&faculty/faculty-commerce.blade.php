fc191aae2969eff0caa2c4363ffd87fa723513d3
Royal-Global-University <ahussain1@rgu.ac>
2026-04-22 11:05:06 +0530

    remove faculty

--- resources/views/frontend/school&faculty/faculty-commerce.blade.php
+++ resources/views/frontend/school&faculty/faculty-commerce.blade.php
@@ -285,18 +285,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://rgu.ac/mobile-assets/updated-faculty-img/Jimpy-Saud.png"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Ms. Jimpy Saud</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-jimpy-saud" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/RSC-prachi-khetan.png"
                                     alt="Profile image">
                             </div>
