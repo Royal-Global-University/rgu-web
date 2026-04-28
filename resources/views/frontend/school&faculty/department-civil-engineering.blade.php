2394b5bb0fd6f3bad0b93b59a4f3a3d7bbd565c0
Royal-Global-University <ahussain1@rgu.ac>
2026-04-28 10:30:08 +0530

    jobs

--- resources/views/frontend/school&faculty/department-civil-engineering.blade.php
+++ resources/views/frontend/school&faculty/department-civil-engineering.blade.php
@@ -335,7 +335,7 @@
                 left: 50%;
                 right: auto;
                 transform: translateX(-50%);
-                width: 85%;
+                width: 60%;
                 /* Wide enough to be prominent, but doesn't touch screen edges */
                 max-width: 350px;
                 justify-content: center;
