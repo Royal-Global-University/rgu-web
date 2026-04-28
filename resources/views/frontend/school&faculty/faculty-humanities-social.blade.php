faee2de8708711b73e0728019de489d2ea564c79
Royal-Global-University <ahussain1@rgu.ac>
2026-04-28 11:44:40 +0530

    faculty add

--- resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
+++ resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
@@ -687,6 +687,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Rugmini-Devi-M.png"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Rugmini Devi M</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="/faculty-krishangi-saikia" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="/mobile-assets/updated-faculty-img/Dr-Sushmita-Nongmeikapam.png"
                                     alt="Profile image">
                             </div>
