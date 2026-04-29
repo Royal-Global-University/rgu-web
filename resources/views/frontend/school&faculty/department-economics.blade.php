e661164a9e1a3e5bec7f0ab2b17cff908bd54089
Royal-Global-University <ahussain1@rgu.ac>
2026-04-29 17:00:46 +0530

    multi media

--- resources/views/frontend/school&faculty/department-economics.blade.php
+++ resources/views/frontend/school&faculty/department-economics.blade.php
@@ -992,7 +992,7 @@
                                                                 <i class="fa fa-download ms-2" style="color: #FF9A1E;"></i>
                                                             </a>
 
-                                                            <a href="mobile-assets/department-all/rshss/economics/updated-syllabus/BA-Economics.pdf"
+                                                            <a href="/mobile-assets/syllabus/RSHSS/BA Economics.pdf"
                                                                 target="_blank" style="color: #27467A;">
                                                                 <i class="fa fa-file-text px-2"></i> Detailed Syllabus -- BA
                                                                 Economics
@@ -1026,13 +1026,13 @@
                                                             <a href="mobile-assets/department-all/rshss/economics/updated-syllabus/PG-Course-structure.pdf"
                                                                 target="_blank" style="color: #27467A;">
                                                                 <i class="fa fa-file-text px-2"></i> Structure of Syllabus
-                                                                -- BA Economics
+                                                                -- MA Economics
                                                                 <i class="fa fa-download ms-2" style="color: #FF9A1E;"></i>
                                                             </a>
 
-                                                            <a href="mobile-assets/department-all/rshss/economics/updated-syllabus/MA-Economics.pdf"
+                                                            <a href="/mobile-assets/syllabus/RSHSS/MA Economics.pdf"
                                                                 target="_blank" style="color: #27467A;">
-                                                                <i class="fa fa-file-text px-2"></i> Detailed Syllabus -- BA
+                                                                <i class="fa fa-file-text px-2"></i> Detailed Syllabus -- MA
                                                                 Economics
                                                                 <i class="fa fa-download ms-2" style="color: #FF9A1E;"></i>
                                                             </a>
