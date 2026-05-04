b498baa69c54ec251bb5e098bfbab1e73fba1f19
Royal-Global-University <ahussain1@rgu.ac>
2026-05-04 13:47:15 +0530

    Forensic Science

--- resources/views/frontend/alllabs/lab-forensic-lab.blade.php
+++ resources/views/frontend/alllabs/lab-forensic-lab.blade.php
@@ -3,7 +3,27 @@
     <div class="mobile">
         @include('frontend/components/mobileheader')
         <div style="padding-top: 90px; position: relative;">
-            <img src="mobile-assets/lab-forensic/mobileForensicScience.png" alt="">
+            <h1 class="text-center pt-2 text-dark fw-bold kd-title-ani kd-split-text">
+                Forensic Science
+            </h1>
+            <img decoding="async" class="p-2 rounded" src="mobile-assets/lab-forensic/headimg.JPG" alt="">
+            <div class="p-3 text-dark">
+                <p class="text-dark para1" style="text-align: justify;">
+                    Step into the fascinating world of forensic science, where science meets
+                    justice! This lab isn't your typical chemistry or biology classroom. Here, the
+                    microscopes unveil hidden clues, fingerprints whisper stories, and DNA unravels
+                    mysteries.
+                </p>
+                <p class="text-dark para1" style="text-align: justify;">
+                    From analyzing blood spatter patterns to extracting evidence from a single hair,
+                    we use cutting-edge technology and meticulous techniques to reveal the truth
+                    behind crimes. We're the silent heroes, speaking for the voiceless and ensuring
+                    the scales of justice tip towards the right side.
+                    The Department of Forensic Science provides its students with a broad knowledge
+                    of forensic science and equips them with the tools and drive to conduct original
+                    independent research in the discipline.
+                </p>
+            </div>
         </div>
     </div>
 
@@ -25,7 +45,8 @@
                                     <div class="kd-about-3-img-wrap txaa-slide-down-1">
                                         <div style="border: 2px solid #fff;"
                                             class="kd-about-3-img img-cover fix kd-img-ani-1">
-                                            <img decoding="async" src="mobile-assets/lab-forensic/headimg.JPG" alt="">
+                                            <img decoding="async" src="mobile-assets/lab-forensic/headimg.JPG"
+                                                alt="">
                                         </div>
 
                                     </div>
@@ -41,7 +62,7 @@
                                 data-id="c8de01e" data-element_type="widget" data-widget_type="go-s-heading.default">
                                 <div class="elementor-widget-container">
                                     <div class="prthalign">
-                                        <h1 class="headd2 text-dark fw-bold kd-title-ani kd-split-text">
+                                        <h1 class="headd1 text-dark fw-bold kd-title-ani kd-split-text">
                                             Forensic Science
                                         </h1>
 
@@ -58,8 +79,7 @@
                                             the scales of justice tip towards the right side.
                                             The Department of Forensic Science provides its students with a broad knowledge
                                             of forensic science and equips them with the tools and drive to conduct original
-                                            independent research in the discipline. The lab is in block- D, room no- D307 on
-                                            the third floor.
+                                            independent research in the discipline.
                                         </p>
 
                                     </div>
@@ -71,34 +91,49 @@
                 </div>
             </section>
 
-            <hr>
+        </div>
+    </div>
+    <div class="custom-container">
+        <div class="custom-row">
+            <div class="custom-column p-4">
+                <h2 style="color: #0056b3 !important;" class="headd1 fw-bold text-dark text-center pb-2 pt-2">
+                    Included In The Lab Are:
+                </h2>
+                <hr>
+                <ol>
+                    <li class="para1 text-dark">Fingerprint development kit</li>
+                    <li class="para1 text-dark">Crime scene management kit</li>
+                    <li class="para1 text-dark">Comparison microscope</li>
+                    <li class="para1 text-dark">Stereo-zoom microscope</li>
+                    <li class="para1 text-dark">Hot air oven</li>
+                    <li class="para1 text-dark">Deep Freezer</li>
+                    <li class="para1 text-dark">Colourimeter</li>
+                    <li class="para1 text-dark">Disarticulated and articulated human skeleton</li>
+                    <li class="para1 text-dark">Models and charts</li>
+                </ol>
+            </div>
+        </div>
+    </div>
 
-            <div class="custom-container">
-                <div class="custom-row">
-                    <div class="custom-column p-4">
-                        <h2 style="color: #0056b3 !important;" class="headd3 fw-bold text-dark text-center pb-2 pt-2">
-                            Included In The Lab Are:
-                        </h2>
-                        <hr>
-                        <ol>
-                            <li class="para1 text-dark">Fingerprint development kit</li>
-                            <li class="para1 text-dark">Crime scene management kit</li>
-                            <li class="para1 text-dark">Comparison microscope</li>
-                            <li class="para1 text-dark">Stereo-zoom microscope</li>
-                            <li class="para1 text-dark">Hot air oven</li>
-                            <li class="para1 text-dark">Deep Freezer</li>
-                            <li class="para1 text-dark">Colourimeter</li>
-                            <li class="para1 text-dark">Disarticulated and articulated human skeleton</li>
-                            <li class="para1 text-dark">Models and charts</li>
-                        </ol>
+    <div class="custom-container">
+        <div class="custom-row">
+            <div class="custom-column p-4">
+                <h2 style="color: #0056b3 !important;" class="headd1 fw-bold text-dark text-center pb-2 pt-2">
+                    Lab Instrument
+                </h2>
+                <div class="row pt-3 pb-3 text-center">
+                    <div class="col-lg-6">
+                        <img style="height: 500px; width: 500px;" src="/mobile-assets/Forensic Science/COMPARISON MICROSCOPE.jpg"/>
+                    </div>
+                    <div class="col-lg-6">
+                        <img style="height: 500px; width: 500px;" src="/mobile-assets/Forensic Science/STEREO MICROSCOPE WITH CAMERA.jpg"/>
                     </div>
                 </div>
             </div>
-
-            <div style="padding: 20px 50px; background-color: #fff;">
-                <img src="mobile-assets/lab-forensic/ForensicScience.png" alt="">
-            </div>
-
         </div>
+    </div>
+
+    <div style="padding: 20px 50px; background-color: #fff;">
+        <img src="mobile-assets/lab-forensic/ForensicScience.png" alt="">
     </div>
 @endsection
