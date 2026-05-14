9985f706245f5dd76f8a2e811fccaf6231bb86a0
Royal-Global-University <ahussain1@rgu.ac>
2026-05-14 14:58:25 +0530

    remove

--- resources/views/frontend/school&faculty/faculty-pharmacy.blade.php
+++ resources/views/frontend/school&faculty/faculty-pharmacy.blade.php
@@ -358,18 +358,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="mobile-assets/updated-faculty-img/Joyjeet.jpeg"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Mr. Joyjeet Dey</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            {{-- <a href="https://www.rgu.ac/himanta-saikia" class="profile-link">View Profile</a> --}}
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="mobile-assets/updated-faculty-img/nayanika.jpeg"
                                     alt="Profile image">
                             </div>
