ead72e8efd0f7cf22c007ac7cbea7a1bee4b06f8
Royal-Global-University <ahussain1@rgu.ac>
2026-05-20 15:57:47 +0530

    new

--- resources/views/frontend/school&faculty/faculty-hotel-management.blade.php
+++ resources/views/frontend/school&faculty/faculty-hotel-management.blade.php
@@ -231,6 +231,19 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Subrata.jpeg"
+                                    alt="Profile image ">
+                            </div>
+                            <h3 class="card-name">Mr. Subrata Pal</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="https://www.rgu.ac/faculty-dikhyant" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Rajkumari.png.png"
                                     alt="Profile image ">
                             </div>
