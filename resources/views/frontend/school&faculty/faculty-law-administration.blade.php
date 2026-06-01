2f0fb02221a720da417c49c7d517610bd52987f1
Royal-Global-University <ahussain1@rgu.ac>
2026-06-01 16:43:33 +0530

    profile

--- resources/views/frontend/school&faculty/faculty-law-administration.blade.php
+++ resources/views/frontend/school&faculty/faculty-law-administration.blade.php
@@ -191,6 +191,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Nita-Nath.jpeg"
+                                    alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Nita Nath</h3>
+                            <p class="card-designation">Associate Professor</p>
+                            {{-- <a href="/faculty-momina-zahan" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/all-faculty/rsla/DrPranita.jpeg"
                                     alt="Profile image">
                             </div>
