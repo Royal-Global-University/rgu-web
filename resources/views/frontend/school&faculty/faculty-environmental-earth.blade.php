f5d407ff7d1d2bf5f4bcd4d419e52e57f3a0e693
Royal-Global-University <ahussain1@rgu.ac>
2026-06-05 14:32:35 +0530

    service provider

--- resources/views/frontend/school&faculty/faculty-environmental-earth.blade.php
+++ resources/views/frontend/school&faculty/faculty-environmental-earth.blade.php
@@ -288,6 +288,18 @@
                         <div class="col-lg-3 col-md-6 mb-4">
                             <div class="card">
                                 <div class="card-image">
+                                    <img src="/mobile-assets/updated-faculty-img/Ayushi.png"
+                                        alt="Profile image">
+                                </div>
+                                <h3 class="card-name">Dr. Ayushi Bhatnagar</h3>
+                                <p class="card-designation">Assistant Professor</p>
+                                {{-- <a href="https://www.rgu.ac/faculty-abhijit-gogoi" class="profile-link">View Profile</a> --}}
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 mb-4">
+                            <div class="card">
+                                <div class="card-image">
                                     <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Neeta-Moni-RSEES.png"
                                         alt="Profile image">
                                 </div>
