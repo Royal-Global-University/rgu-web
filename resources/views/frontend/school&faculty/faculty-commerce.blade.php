61fea7635b322fc67da132a4ab1294820af1e3c7
ayurat-ai <ayurat093@gmail.com>
2026-07-21 12:55:41 +0530

    new data

--- resources/views/frontend/school&faculty/faculty-commerce.blade.php
+++ resources/views/frontend/school&faculty/faculty-commerce.blade.php
@@ -503,18 +503,6 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Sampada.jpeg"
-                                    alt="Profile image">
-                            </div>
-                            <h3 class="card-name">Dr. Sampada Tiwari</h3>
-                            <p class="card-designation">Assistant Professor</p>
-                            <a href="https://www.rgu.ac/faculty-sampada" class="profile-link">View Profile</a>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 mb-4">
-                        <div class="card">
-                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Santosh-K-Anand-RSB.png"
                                     alt="Profile image">
                             </div>
