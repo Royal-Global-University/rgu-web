c02ca94f516e6d1fbe428f7828687fec02927881
ayurat-ai <ayurat093@gmail.com>
2026-07-25 17:41:52 +0530

    new

--- resources/views/frontend/school&faculty/faculty-law-administration.blade.php
+++ resources/views/frontend/school&faculty/faculty-law-administration.blade.php
@@ -114,7 +114,7 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="/mobile-assets/updated-faculty-img/Souvik Chatterji.png"
+                                <img src="/mobile-assets/updated-faculty-img/Souvik-Chatterji.jpeg"
                                     alt="Profile image">
                             </div>
                             <h3 class="card-name">Prof. (Dr.) Souvik Chatterji</h3>
