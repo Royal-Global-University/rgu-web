115cb0d512fe1d4aed6d988901f80dc6ff948dd7
ayurat-ai <ayurat093@gmail.com>
2026-07-28 17:26:45 +0530

    new pages

--- resources/views/frontend/school&faculty/faculty-environmental-earth.blade.php
+++ resources/views/frontend/school&faculty/faculty-environmental-earth.blade.php
@@ -204,6 +204,18 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Dr. Md Syed Salimuddin.jpeg"
+                                    alt="Profile image ">
+                            </div>
+                            <h3 class="card-name">Dr. Md Syed Salimuddin</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="https://www.rgu.ac/faculty-hibjur-rahaman" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="https://www.rgu.ac/mobile-assets/updated-faculty-img/Palme-Borthakur.png"
                                     alt="Profile image ">
                             </div>
