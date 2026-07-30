052e2da123db494bdfdf7c0b8383a30e8b801ba7
ayurat-ai <ayurat093@gmail.com>
2026-07-30 19:00:31 +0530

    new pages

--- resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
+++ resources/views/frontend/school&faculty/faculty-humanities-social.blade.php
@@ -863,6 +863,17 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Shivangi.png"
+                                    alt="Profile image of Tripti Das">
+                            </div>
+                            <h3 class="card-name">Dr. Shivangi Kaushik</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="/mobile-assets/updated-faculty-img/Mr-Kadiguang-Panmei.png"
                                     alt="Profile image ">
                             </div>
