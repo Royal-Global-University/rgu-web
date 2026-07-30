c42462579609205a96a321b27b52cdefd9bc7a11
ayurat-ai <ayurat093@gmail.com>
2026-07-30 15:24:42 +0530

    new pages

--- resources/views/frontend/school&faculty/faculty-information-technology.blade.php
+++ resources/views/frontend/school&faculty/faculty-information-technology.blade.php
@@ -170,6 +170,17 @@
                     </div>
 
                     <div class="col-lg-3 col-md-6 mb-4">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img src="/mobile-assets/updated-faculty-img/Daisy-Das.png"
+                                        alt="Profile image">
+                                </div>
+                                <h3 class="card-name">Dr. Daisy Das</h3>
+                                <p class="card-designation">Assistant Professor</p>
+                            </div>
+                        </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
                                 <img src="/mobile-assets/updated-faculty-img/Dillip.jpeg"
