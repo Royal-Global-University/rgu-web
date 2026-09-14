ba6bf22852f300443659812fe1b66ae5cfa1dd61
dev-aster-ai <devasterai@gmail.com>
2026-09-14 10:13:36 +0530

    new calender

--- resources/views/frontend/school&faculty/faculty-nursing.blade.php
+++ resources/views/frontend/school&faculty/faculty-nursing.blade.php
@@ -404,7 +404,7 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
-                                <img src="/mobile-assets/updated-faculty-img/Prity.png" alt="Profile image">
+                                <img src="/mobile-assets/updated-faculty-img/nutrition/Prity.png" alt="Profile image">
                             </div>
                             <h3 class="card-name">Ms. Prity Rekha Talukdar</h3>
                             <p class="card-designation">Lecturer/Tutor</p>
