68c67495fab2dd71fcc34f328635abe2ec40c0ab
dev-aster-ai <devasterai@gmail.com>
2026-09-21 17:25:23 +0530

    anti

--- resources/views/frontend/school&faculty/faculty-business.blade.php
+++ resources/views/frontend/school&faculty/faculty-business.blade.php
@@ -225,6 +225,17 @@
                     <div class="col-lg-3 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Mohammed.png" alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Mohammed Samroodh T.V.</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="/faculty-abheeshek-dev-roye" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
                                 <img src="/mobile-assets/updated-faculty-img/Neha-Tiwari.png" alt="Profile image">
                             </div>
                             <h3 class="card-name">Dr. Neha Tiwari</h3>
--- resources/views/frontend/school&faculty/faculty-commerce.blade.php
+++ resources/views/frontend/school&faculty/faculty-commerce.blade.php
@@ -202,6 +202,17 @@
                         </div>
                     </div>
 
+                    <div class="col-lg-3 col-md-6 mb-4">
+                        <div class="card">
+                            <div class="card-image">
+                                <img src="/mobile-assets/updated-faculty-img/Mohammed.png" alt="Profile image">
+                            </div>
+                            <h3 class="card-name">Dr. Mohammed Samroodh T.V.</h3>
+                            <p class="card-designation">Assistant Professor</p>
+                            {{-- <a href="/faculty-abheeshek-dev-roye" class="profile-link">View Profile</a> --}}
+                        </div>
+                    </div>
+
                     <!-- Assistant Professors (A–Z) -->
 
                     <div class="col-lg-3 col-md-6 mb-4">
